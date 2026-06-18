21c89a5b45fd2a9c4c8ff6e7cc4272525742cfa3
Arnesh Upadhyaya <arneshupadhyaya@gmail.com>
2026-06-18 19:35:28 +0530

    Update contrastive_loss.py

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -4,49 +4,19 @@ import torch.nn.functional as F
 
 
 class ContrastiveLoss(nn.Module):
-
-    def __init__(
-        self,
-        margin=2.0
-    ):
-
+    def __init__(self, margin=2.0):
         super().__init__()
-
         self.margin = margin
 
-    def forward(
-        self,
-        output1,
-        output2,
-        label
-    ):
+    def forward(self, output1, output2, label):
+        distance = F.pairwise_distance(output1, output2)
 
-        distance = F.pairwise_distance(
-            output1,
-            output2
+        positive_loss = (1 - label) * distance.pow(2)
+
+        negative_loss = label * (
+            torch.clamp(self.margin - distance, min=0.0).pow(2)
         )
 
-        loss = torch.mean(
-
-            (1 - label)
-            * torch.pow(
-                distance,
-                2
-            )
-
-            +
-
-            label
-            * torch.pow(
-
-                torch.clamp(
-                    self.margin
-                    - distance,
-                    min=0.0
-                ),
-
-                2
-            )
-        )
+        loss = torch.mean(positive_loss + negative_loss)
 
         return loss
